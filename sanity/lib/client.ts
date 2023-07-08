a34459d2015de60fcb9f2ec17aaa5881c54aeb38
alae tabet <alta.tabet@gmail.com>
2023-07-08 13:27:24 +0200

    update

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,7 +6,7 @@ export const client = createClient({
   apiVersion,
   dataset,
   projectId,
-  useCdn: true,
-  apiHost: process.env.NEXT_PUBLIC_BASE_URL || "https://ecom-ten-gray.vercel.app",
+  useCdn,
+  //apiHost: process.env.NEXT_PUBLIC_BASE_URL || "https://ecom-ten-gray.vercel.app",
   //token: "skf9CboCyKsCdHF8MoncRHIC0vB4FggXkfap754xuPaDsJOnpAkVSLexpDfYIk2HGprCVeUyvMVCG2rFBMYTszYPLUoa0eCUSsnGx2WWVZxSYg18fw5zEEqYfPIOtssM9v05z4Ky1uGWLKgPazELYa2qWepd6vTMCfJVgM0d2onNzhwf3r3V",
 })
