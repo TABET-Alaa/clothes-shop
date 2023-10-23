c52753ea5e66d30b699bc57d713bf68b58ecaf5c
TABET-Alaa <alta.tabet@gmail.com>
2023-10-23 18:18:44 +0200

    update

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -2,10 +2,9 @@ import { NextResponse } from "next/server";
 // @ts-ignore
 import { formatLineItems } from "use-shopping-cart/utilities"
 import { inventory } from "@/config/inventory"
-import Stripe from "stripe";       
+// import Stripe from "stripe";       
 import stripe from "@/app/stripeConfig";
 //import { stripe } from "@/lib/stripe"
-
 
 export async function POST(request: Request) {
     /* */
